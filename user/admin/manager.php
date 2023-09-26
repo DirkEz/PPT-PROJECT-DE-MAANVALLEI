27ac190f9c13adde047b79c881bdea825018a9e9
DirkEz <dirk.schaafstra@gmail.com>
2023-09-26 14:30:36 +0200

    Create manager.php

---
+++ user/admin/manager.php
@@ -0,0 +1,26 @@
+<?php 
+
+include_once "../../layout/session.php";
+
+if (!isset($_SESSION['loggedin'])) {
+    if ($_SESSION['positie_id'] != 2) {
+        header('../login.php');
+        exit;
+    } else {
+        echo "hoi!";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Manager
+    <a href="../logout.php">Uitloggen</a>
+</body>
+</html>
