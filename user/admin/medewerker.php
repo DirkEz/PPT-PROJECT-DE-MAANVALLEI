33dc73b033aeaa470baa375dee58c5ccac45ee09
DirkEz <dirk.schaafstra@gmail.com>
2023-09-26 14:30:38 +0200

    Create medewerker.php

---
+++ user/admin/medewerker.php
@@ -0,0 +1,22 @@
+<?php 
+
+include_once "../../layout/session.php";
+
+if ($_SESSION['positie_id'] != 1) {
+    header('../login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    admin
+    <a href="../logout.php">Uitloggen</a>
+</body>
+</html>
