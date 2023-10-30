93034cd48485aed9c9b49f2d35f21cdcff22bf52
MC1905 <94351375+MC1905@users.noreply.github.com>
2023-10-30 14:48:56 +0100

    klachten (error nog niet opgelost)
    -

--- user/user/klachten/verwerk_klacht.php
+++ user/user/klachten/verwerk_klacht.php
@@ -1,25 +1,32 @@
 <?php
+include('config.php'); // Include your database configuration
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $klant_id = $_POST["klant_id"];
     $titel_klacht = $_POST["titel_klacht"];
     $klacht = $_POST["klacht"];
 
-    // Maak een databaseverbinding
-    $connection = new mysqli("localhost", "gebruikersnaam", "wachtwoord", "databasenaam");
+    // Create a new MySQLi connection using the included database configuration
+    $connection = new mysqli($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 
     if ($connection->connect_error) {
         die("Verbindingsfout: " . $connection->connect_error);
     }
 
-    // Voer de klacht in de database in
-    $query = "INSERT INTO klachten (klant_id, titel_klacht, klacht) VALUES ('$klant_id', '$titel_klacht', '$klacht')";
+    // Use prepared statements to insert data
+    $query = $connection->prepare("INSERT INTO klachten (klant_id, titel_klacht, klacht) VALUES (?, ?, ?");
+    
+    // Bind parameters
+    $query->bind_param("iss", $klant_id, $titel_klacht, $klacht);
 
-    if ($connection->query($query) === TRUE) {
+    if ($query->execute()) {
         echo "Klacht is succesvol ingediend.";
     } else {
         echo "Fout bij het indienen van de klacht: " . $connection->error;
     }
 
+    // Close the prepared statement and the connection
+    $query->close();
     $connection->close();
 }
 ?>
