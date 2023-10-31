45023e3f1dbe6d449eb41a70dd8143abe28dd5e9
bjoreno <bjodegens@hotmail.com>
2023-10-31 12:08:12 +0100

    edit beheer_werknemer en kampeerplaatsen_inzien met bootstrap

--- user/admin/kampeerplaatsen_inzien.php
+++ user/admin/kampeerplaatsen_inzien.php
@@ -1,14 +1,12 @@
 <?php
-
-
 include_once "../user-creation/config/config.php";
 include_once "../../layout/session.php";
 
 if ($_SESSION['loggedin'] == TRUE) {
-    if ($_SESSION['positie_id'] != 1 && $_SESSION['positie_id'] != 2 ) {
+    if ($_SESSION['positie_id'] != 1 && $_SESSION['positie_id'] != 2) {
         header('location:../login.php');
     }
-}else header('location:../login.php');
+} else header('location:../login.php');
 
 $sql = "SELECT * FROM kampeerplaatsen";
 $stmt = $connect->prepare($sql);
@@ -20,41 +18,63 @@ $stmt = $connect->prepare($sql);
 $stmt->execute();
 $accounts = $stmt->fetchAll();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Kampeerplaatsen List</title>
+    <!-- Add Bootstrap CSS Link -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <a href="manager.php">Go back</a>
 
-<h2>kampeerplaatsen</h2>
-<?php foreach ($kampeerplaats_result as $kampeerplaats): ?>
-    <form action="config/kampeerplaatsen_bewerken.php" method="post">
-        <p>
-            <input name="id" value="<?= $kampeerplaats['id'] ?>" readonly>
-            <input name="kampeernummer" value="<?= $kampeerplaats['kampeerplek_nummer']; ?>" readonly>
-            <select name="klant_id">
-                <option value="">-- Selecteer een account --</option> <!-- Default empty option -->
-                <?php foreach ($accounts as $account): ?>
-                    <option value="<?= $account['id'] ?>" <?= ($kampeerplaats['klant_id'] == $account['id']) ? 'selected' : '' ?>>
-                        <?= $account['voornaam'] ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
+<div class="container">
+    <h2 class="mt-4">Kampeerplaatsen</h2>
+    <ul class="list-group mt-3">
+        <?php foreach ($kampeerplaats_result as $kampeerplaats): ?>
+            <li class="list-group-item">
+                <form action="config/kampeerplaatsen_bewerken.php" method="post">
+                    <div class="form-row">
+                        <div class="form-group col-md-1">
+                            <input name="id" value="<?= $kampeerplaats['id'] ?>" class="form-control" readonly>
+                        </div>
+                        <div class="form-group col-md-2">
+                            <input name="kampeernummer" value="<?= $kampeerplaats['kampeerplek_nummer']; ?>" class="form-control" readonly>
+                        </div>
+                        <div class="form-group col-md-3">
+                            <select name="klant_id" class="form-control">
+                                <option value="">-- Selecteer een account --</option>
+                                <?php foreach ($accounts as $account): ?>
+                                    <option value="<?= $account['id'] ?>" <?= ($kampeerplaats['klant_id'] == $account['id']) ? 'selected' : '' ?>>
+                                        <?= $account['voornaam'] ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        <div class="form-group col-md-1">
+                            <input name="betaald" value="<?= $kampeerplaats['betaald']; ?>" class="form-control">
+                        </div>
+                        <div class="form-group col-md-1">
+                            <input name="bezet" value="<?= $kampeerplaats['bezet']; ?>" class="form-control">
+                        </div>
+                        <div class="form-group col-md-1">
+                            <input name="schoongemaakt" value="<?= $kampeerplaats['schoongemaakt']; ?>" class="form-control">
+                        </div>
+                        <div class="form-group col-md-2">
+                            <button name="update" type="submit" class="btn btn-primary">Update</button>
+                            <button name="reset" type="submit" class="btn btn-danger">Reset</button>
+                        </div>
+                    </div>
+                </form>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
 
-            </select>
-            <input name="betaald" value="<?= $kampeerplaats['betaald']; ?>">
-            <input name="bezet" value="<?= $kampeerplaats['bezet']; ?>">
-            <input name="schoongemaakt" value="<?= $kampeerplaats['schoongemaakt']; ?>">
-            <button name="update" type="submit">update</button>
-            <button name="reset" type="submit">reset</button>
-        </p>
-    </form>
-<?php endforeach; ?>
-
-
+<!-- Add Bootstrap JS Link -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
