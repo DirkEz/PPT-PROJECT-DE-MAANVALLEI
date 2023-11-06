2fbcc6d20b33b90cd78ba5a6c6362bcc672a5344
DirkEz <dirk.schaafstra@gmail.com>
2023-11-06 14:50:22 +0100

    Update klacht_formulier.php

--- user/user/klachten/klacht_formulier.php
+++ user/user/klachten/klacht_formulier.php
@@ -6,13 +6,14 @@
 <body>
     <h1>Klacht indienen</h1>
     <form method="post" action="verwerk_klacht.php">
-        <label for="klant_id">Klant ID:</label><br>
-        <input type="text" name="klant_id" id="klant_id"><br><br>
+        <label for="klant_id">Klant Naam:</label><br>
+        <input type="text" name="klant_naam" id="klant_naam"><br><br>
         <label for="titel_klacht">Titel klacht:</label><br>
         <input type="text" name="titel_klacht" id="titel_klacht"><br><br>
         <label for="bericht">Klacht bericht:</label><br>
         <textarea name="bericht" id="bericht" rows="4" cols="50"></textarea><br><br>
-        <input type="submit" value="Indienen">
+        <input name="klacht_indienen" type="submit" value="Indienen">
     </form>
+    <a href="./klachten.php"><button>Klachten Bekijken</button></a>
 </body>
 </html>
