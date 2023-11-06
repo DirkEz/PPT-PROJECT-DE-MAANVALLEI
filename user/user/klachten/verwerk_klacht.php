966aec836d4a52f680462fa49462bb08e6e0f10c
DirkEz <dirk.schaafstra@gmail.com>
2023-11-06 14:50:13 +0100

    Update verwerk_klacht.php
    Normale backend toegevoegd

--- user/user/klachten/verwerk_klacht.php
+++ user/user/klachten/verwerk_klacht.php
@@ -1,38 +1,19 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $klant_id = $_POST["klant_id"];
-    $titel_klacht = $_POST["titel_klacht"];
-    $bericht = $_POST["bericht"];
-
-    // Replace with your actual database credentials
-    $host = "localhost";
-    $username = "root";  // Replace with your database username
-    $password = "";      // Replace with your database password
-    $database = "ppt_maasvallei";
-
-    // Create a new MySQLi connection
-    $connection = new mysqli($host, $username, $password, $database);
-
-    if ($connection->connect_error) {
-        die("Verbindingsfout: " . $connection->connect_error);
-    }
-
-    // Use prepared statements to insert data
-    $query = $connection->prepare("INSERT INTO klachten (klant_id, titel_klacht, bericht, verwerkt) VALUES (?, ?, ?, ?)");
-
-
+<?php 
+    require_once('../../config/config.php');
     
-    // Bind parameters
-    $query->bind_param("iss", $klant_id, $titel_klacht, $klacht);
+    if (isset($_POST['klacht_indienen'])){
+        $sql = "INSERT INTO klachten (klant_naam, titel_klacht, bericht)
+        VALUES (:klant_naam,:titel_klacht,:bericht)";
+        
+        $stmt = $con->prepare($sql);
 
-    if ($query->execute()) {
-        echo "Klacht is succesvol ingediend.";
+        $stmt->bindParam(":klant_naam", $_POST['klant_naam']);
+        $stmt->bindParam(":titel_klacht", $_POST['titel_klacht']);
+        $stmt->bindParam(":bericht", $_POST['bericht']);
+        $stmt->execute();
+
+        header("Location: ../klachten.php");
     } else {
-        echo "Fout bij het indienen van de klacht: " . $connection->error;
+        header("Location: ../klacht_formulier.php");
     }
-
-    // Close the prepared statement and the connection
-    $query->close();
-    $connection->close();
-}
 ?>
