e19a802353a31d07cf4e4471ee7d1a0db09b0ec7
DirkEz <dirk.schaafstra@gmail.com>
2023-11-06 14:59:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
     <title>Home</title>
 </head>
 <body>
+<<<<<<< Updated upstream
 <a href="user\login.php">login</a>
     <a href="user\register.php">register</a>
+=======
+    <header>
+        <h1>Maasvallei</h1>
+    </header>
+    <nav>
+        <a href="user/login.php">Login</a>
+        <a href="user/register.php">Register</a>
+        <!-- <a href="userpage.php">User Page</a> -->
+        <a href="user/user/klachten/klacht_formulier.php">Klachten</a>
+    </nav>
+>>>>>>> Stashed changes
 </body>
 </html>
